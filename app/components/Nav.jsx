fa37e3953b89d8b6d3aff8dcb42e595b42bb8726
ChavyChaze <sobolrip1@gmail.com>
2019-07-25 15:31:59 +0300

    adding routes

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,11 +1,17 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const Nav = React.createClass({
-  render: function () {
-    return (
-      <h2>Navigation Component</h2>
-    );
-  }
+    render: function () {
+        return (
+            <div>
+                <h2>Navigation Component</h2>
+                <Link to="/">Get Weather</Link>
+                <Link to="/about">About</Link>
+                <Link to="/examples">Examples</Link>
+            </div>
+        );
+    }
 });
 
 module.exports = Nav;
